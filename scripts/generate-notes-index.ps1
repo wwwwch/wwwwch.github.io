e3cbf00d4d0b51d065ed6dcec660a1fb5a66ab43
2837490993@qq.com <2837490993@qq.com>
2026-06-06 23:14:27 +0800

    260606

--- scripts/generate-notes-index.ps1
+++ scripts/generate-notes-index.ps1
@@ -13,19 +13,7 @@ foreach ($dirName in $noteDirs) {
 
     $notes = @(Get-ChildItem -LiteralPath $htmlDir -Filter "*.html" -File |
         ForEach-Object {
-            $content = Get-Content -LiteralPath $_.FullName -Raw -Encoding UTF8
-            $titleMatch = [regex]::Match(
-                $content,
-                "<title[^>]*>(.*?)</title>",
-                [System.Text.RegularExpressions.RegexOptions]::IgnoreCase -bor
-                [System.Text.RegularExpressions.RegexOptions]::Singleline
-            )
-
-            $title = if ($titleMatch.Success) {
-                [System.Net.WebUtility]::HtmlDecode(($titleMatch.Groups[1].Value -replace "\s+", " ").Trim())
-            } else {
-                $_.BaseName
-            }
+            $title = $_.BaseName
 
             $numberMatch = [regex]::Match($_.Name, "^\d+")
             $number = if ($numberMatch.Success) { [int]$numberMatch.Value } else { $null }
